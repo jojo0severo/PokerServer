47efe5a6bed62ab73381eff33d63dcd84999ebc6
jojo0severo <joaovbsevero@gmail.com>
2018-11-12 14:05:07 -0200

    Pequenas alterações
    Comentários, funcionalidade de administrador e pequenas correções foram feitas para se adequar ao projeto

--- Clients/game_client.py
+++ Clients/game_client.py
@@ -1,25 +1,15 @@
 from socketIO_client import SocketIO, LoggingNamespace
-import sys
 
-sys.setrecursionlimit(10000)
-connected = False
+
+all_users_connected = False
 all_players_played = False
+name = ''
 
 
+# ======= CONNECT WITH THE SERVER AND GET THE INFORMATION
 def on_receive_hand(message):
     [print(value) for value in message]
     print('\n')
-
-
-def bet_received(message):
-    global all_players_played
-    all_players_played = True
-    print(message + '\n\n')
-
-
-def on_users_connected(message):
-    global connected
-    connected = True
 
 
 def on_response(message):
@@ -29,7 +19,6 @@ def on_response(message):
         exit(-1)
 
 
-# connection
 def connection():
     socketIO.on('response', on_response)
     socketIO.on('cards', on_receive_hand)
@@ -37,19 +26,43 @@ def connection():
     socketIO.wait(5)
 
 
+#  ============ WAITS FOR ALL THE PLAYERS TO CONNECT =========
+def on_users_connected(message):
+    global all_users_connected
+    all_users_connected = True
+
+    print(message + '\n\n')
+
+
 def wait_users():
     socketIO.on('users_connected', on_users_connected)
     socketIO.emit('wait_connections', name)
-    while not connected:
-        socketIO.wait(5)
+
+    while not all_users_connected:
+        socketIO.wait(1)
 
 
-def wait_plays(bet):
-    socketIO.on('plays_done', bet_received)
-    socketIO.emit('play', {'play': 'bet', 'name': name, 'bet': bet})
+# =========== WAITS FOR ALL THE PLAYERS TO PLAY ===============
+def bet_received(message):
+    global all_players_played
+    all_players_played = True
+    print(message + '\n\n')
+
+
+def wait_plays(kind, bet):
+    if kind == 'bet':
+        socketIO.on('plays_done', bet_received)
+        socketIO.emit('play', {'type': 'bet', 'name': name, 'data': bet})
+
+    else:
+        socketIO.on('plays_done', bet_received)
+        socketIO.emit('play', {'type': 'quit', 'name': name, 'data': None})
+
     while not all_players_played:
-        socketIO.wait(3)
+        socketIO.wait(1)
 
+
+# ============ SHOW THE OPTIONS OF EACH MATCH ================
 
 def match():
     while True:
@@ -57,24 +70,24 @@ def match():
                        '	If you want to bet (will lose anyway, just know it): 2\n'
                        '	If you are pissed with the game (extremaly pussy): 3\n')
 
-        if option == '2':
-            bet = input('Gimme your bet, motherfucker:\n')
-            print('Waiting for other motherfucker players')
-            wait_plays(bet)
-
-        elif option == '1':
+        if option == '1':
             print('Just know that you are a pussy, motherfucker')
             socketIO.emit('remove_player', name)
-            socketIO.wait(2)
+            socketIO.wait(5)
             print('Say the exit code slowly and out loud')
             print('Process finished with exit code 104')
             exit(0)
+
+        elif option == '2':
+            bet = input('Gimme your bet, motherfucker:\n')
+            print('Waiting for other motherfucker players')
+            wait_plays('bet', bet)
 
         elif option == '3':
             print('I don´t give a fuck')
             socketIO.emit('remove_money', name)
             print('Just removed 300 from your wallet, bitch')
-            socketIO.wait(3)
+            socketIO.wait(5)
 
         else:
             print('Are you stupid?\nI guess you are...\nChoose a valid option -_-')
